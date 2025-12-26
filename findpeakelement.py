0bc7f3ee5dde6d0b876fe9501b8b3505b6829ef8
HemanthM18 <141708170+HemanthM18@users.noreply.github.com>
2025-12-26 10:29:17 +0530

    Problem find peak element is done
    Implement binary search to find a peak element in an array.

---
+++ findpeakelement.py
@@ -0,0 +1,15 @@
+//https://takeuforward.org/plus/dsa/problems/find-peak-element
+
+class Solution:
+    def findPeakElement(self, arr):
+        low, high = 0, len(arr) - 1
+        while low < high:
+            mid = (low + high) // 2
+            if arr[mid] < arr[mid+1]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
+
+
+
